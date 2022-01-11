30d2769008e74171674851d3a05207ee3b136b03
Hameedreza Gucci <hamidguc96@gmail.com>
2022-01-11 12:45:51 +0330

    fix some errors

--- server/app.js
+++ server/app.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const hbs = require('hbs');
 const request = require('postman-request');
+const path = require('path');
 
 
+const publicPath = path.join(__dirname , '../public');
+const viewPath = path.join(__dirname , 'views');
+console.log(viewPath);
 
 const app = express();
 
 
+app.use(express.static(publicPath));
+app.set('view engine' , 'hbs');
+app.set('views' , viewPath);
+
+
+
+app.get('/' , (req , res)=>{
+  res.render('index');
+})
 
 
 
